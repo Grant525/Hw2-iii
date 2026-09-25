d343ecd6b434d17264b7df5ebdda38b87afb412d
thee28 <teetat2810@gmail.com>
2026-09-25 15:30:16 -0400

    uarray2.h done

---
+++ bit2.h
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <string.h>
+#include "assert.h"
+#include "bit.h"
+
+#ifndef BIT2_INCLUDED
+#define BIT2_INCLUDED
+
+#define T Bit2_T   
+typedef struct T *T;
+
+
+
+#undef T
+#endif
--- uarray2.h
+++ uarray2.h
@@ -1,24 +1,33 @@
-#ifndef UARRAY2_H_  
-#define UARRAY2_H_
+#include <stdlib.h>
+#include <string.h>
+#include "assert.h"
+#include "uarray.h"
 
-#define T UArray2_T     //does this go here?
+#ifndef UARRAY2_INCLUDED
+#define UARRAY2_INCLUDED
+
+#define T UArray2_T   
+typedef struct T *T;
 
 /* uarray2_new
 * Summary: 
-* Allocates space for and creates a new 2D array, using the given width, height,
-* and size of each element
+* Allocates space for and creates a new 2D array, using the given width, 
+* height, and size of each element
+*
 * Parameters:
-*         const int WIDTH: width of the 2D array
-*	        const int HEIGHT: height of the 2D array
-* 	      const int ELEMENT_SIZE: byte size of each element in the array
+*        const int width: width of the 2D array
+*        const int height: height of the 2D array
+* 	     const int size: byte size of each element in the array
+*
 * Return: A pointer to the 2D array
 * 
-* Expects: WIDTH, HEIGHT, and ELEMENT_SIZE should be greater than 0 than 0
+* Expects: The newly allocated memory is not NULL
 */                                                                                
-T urray2_new(unsigned int width, unsigned int height, unsigned int ELEMENT_SIZE)
+T Urray2_new(unsigned int width, unsigned int height, unsigned int size)
 
-/* NAME: uarray2_width(T array)
-* Summary: Goes through the 2Darray and returns the number of columns (or length of each row)
+/* uarray2_width(T array)
+* Summary: Goes through the 2Darray and returns the number of columns 
+*        (or length of each row)
 * 
 * Parameters: T array: pointer to the 2D array
 *
@@ -27,75 +36,81 @@ T urray2_new(unsigned int width, unsigned int height, unsigned int ELEMENT_SIZE)
 * Expects: The 2D array is not empty. All columns are the same length as each
 *          other. All rows are the same length as each other. 
 */
-int uarray2_width(T array)
+int Uarray2_width(T uarray2)
 
 /* NAME
-* Summary: Goes through the 2Darray and returns the number of rows (or length of each column)
+* Summary: Goes through the 2Darray and returns the number of rows 
+*        (or length of each column)
 * 
-* Parameters: T array: pointer to the 2D array
+* Parameters: T uarray2: pointer to the 2D array
 *
-* Return: Returns an integer representing the number of rows in the array 
+* Return: Returns an integer representing the number of rows in the array
 *
-* Expects: The 2D array is not empty.  All columns are the same length as each other. All
-*          rows are the same length as each other. 
+* Expects: The 2D array is not empty.  All columns are the same length as each
+*         other. All rows are the same length as each other. 
 */
-int uarray2_height(T array)
+int Uarray2_height(T uarray2)
 
 /* uarray2_size
 * Summary: Check for the size each element is taking up in the array
 * 
-* Parameters: T array: pointer to the 2D array
+* Parameters: T uarray2: pointer to the 2D array
 *
 * Return: Returns an integer representing the size of each element in the array
 *
 * Expects: The 2D array is not empty (array is not null)
 *
 */
-int uarray2_size(T array)
+int Uarray2_size(T uarray2)
 
 /* uarray2_at
-* Summary: Go through the array and return the element associated with the given width and 
-height coordinate
+* Summary: Go through the array and return the element associated with the
+*        given width and height coordinate
 * 
-* Parameters: T array: pointer to the 2D array
+* Parameters: T uarray2: pointer to the 2D array
 *         unsigned int width: width position of the desired element
 *         unsigned int height:  height position of the desired element
 *
-* Return: pointer value of the element in the 2D array at the given width and height (x,y)
+* Return: pointer value of the element in the 2D array at the given width
+*        and height (x,y)
 * 
 * Expects: The 2D array is not empty (array is not null)
 *
 */
-void *uarray2_at (T array, unsigned int width, unsigned int height)
+void *Uarray2_at (T uarray2, unsigned int width, unsigned int height)
 
 /* uarray2_map_col_major
 * Summary: Maps an apply function to each element in the array row by row 
 * 
 * Parameters: T array: pointer to the 2D array
-*                      Apply () function 
-*	           *closure
-* Return: Returns the result of apply() for each element of the array row by row
+*       Apply () function 
+*	    *cl: pointer to bool that checks invariants
+* Return: Nothing
 *
 * Expects:  The 2D array is not empty.  All columns are the same length as each other.
 * All rows * are the same length as each other. Apply() takes in an array and returns
 * some value. 
 */
-uarray2_map_col_major (array, apply(), *cl)
+void Uarray2_map_col_major (T uarray2, 
+        void apply((int col, int row, T uarray2, void *elem, void *cl)),
+        *cl)
 
-/* NAME
+/* urray2_map_row_major
 * Summary: Maps an apply function to each element in the array column by column 
 * 
 * Parameters: T array: pointer to the 2D array
-*                      Apply () function 
-*	           *closure
+*       Apply () function 
+*	    *cl: pointer to bool that checks invariants
 *
-* Return:  Returns the result of apply() for each element of the array col by col*
+* Return: Nothing
 *
 * Expects:  The 2D array is not empty.  All columns are the same length as each other.
 * All rows * are the same length as each other. Apply() takes in an array and returns
 * some value. 
 */
-urray2_map_row_major (array, apply(), *cl)
+void Urray2_map_row_major (T uarray2, 
+        void apply((int col, int row, T uarray2, void *elem, void *cl)),
+        *cl)
 
 /* uarray2_free
 * Summary: Deallocates all of the allocated memory for the 2D array.
@@ -105,8 +120,8 @@ urray2_map_row_major (array, apply(), *cl)
 * Return: nothing
 *
 * Expects: The 2D array is not empty (array is not null) and has allocated memory. 
-*	
 */
-void urray2_free(T array)
+void Urray2_free(T uarray2)
 
+#undef T
 #endif
